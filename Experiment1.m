424ae74688fc6c5605274103f6ea89284d5255f6
ttake <ttake@LAPTOP-PVKHKF5J>
2025-05-14 10:14:14 +0900

    add files

---
+++ Experiment1.m
@@ -0,0 +1,42 @@
+% Observation of flow in a circular pipe and measurement of pipe friction loss coefficient
+clc; close all; clear;
+
+%% setting condition
+addpath(genpath('ToolboxForSimulator'));
+ExpeData_Inc   = readtable('ExperimentalData_Increase.xlsx');
+ExpeData_Dec   = readtable('ExperimentalData_Decrease.xlsx');
+Dyn_Viscosity  = readtable('Dyn_Viscosity.xlsx');
+
+importfolder   = 'ToolboxForSimulator/condition';
+importfilename = 'Condition.xlsx'; % 条件のファイル
+Data           = dataimport(importfolder,importfilename,[]);
+Data.Point0Cor = Data.Condition.Manometer.Upstream - Data.Condition.Manometer.Downstream;
+disp('ゼロ点補正値：'); disp(Data.Point0Cor);
+CriRe_Inc      = 6045; % Increase Critical Reynolds number
+CriRe_Dec      = 6030; % Decrease Critical Reynolds number
+
+%% 2原理
+% Calculate Reynolds
+VarFluid_LogInc = calc_ReLam(Data, ExpeData_Inc, Dyn_Viscosity, CriRe_Inc); % Increase
+VarFluid_Logdec = calc_ReLam(Data, ExpeData_Dec, Dyn_Viscosity, CriRe_Dec); % Decrease
+
+%% 結果を表示・プロット
+Fontsize_num = 10.5;
+OutputName_Inc = '増加過程'; OutputName_Dec = '減少過程';
+
+% § 5.6
+plot_ReFri_Comp(VarFluid_LogInc, VarFluid_Logdec, Fontsize_num, OutputName_Inc, OutputName_Dec)
+
+% § 5.8
+plot_ReFri(VarFluid_LogInc, Fontsize_num, OutputName_Inc)
+plot_ReFri(VarFluid_Logdec, Fontsize_num, OutputName_Dec)
+
+% § 5.16
+max_rel_error_Inc = max(VarFluid_LogInc.RelErr, [], 'omitnan');
+mean_rel_error_Inc = mean(VarFluid_LogInc.RelErr, 'omitnan');  % 全体の相対誤差の平均
+disp(max_rel_error_Inc)
+disp(mean_rel_error_Inc)
+
+% 
+% figure;
+% plot(VarFluid_LogInc.Vm, VarFluid_LogInc.Re, 'o')
---
+++ ToolboxForSimulator/CalculationToolbox/calc_VisCoeff.m
@@ -0,0 +1,28 @@
+function VarFluid = calc_VisCoeff(Data, ExpeData, Dyn_Viscosity)
+
+VarFluid.mu_mPas = calc_DynViscosity(ExpeData, Dyn_Viscosity); % Calculate DynViscosity
+mu_Pas           = VarFluid.mu_mPas * 1e-3;   % mPa·s を Pa·s に変換
+VarFluid.rho     = calc_Density(Data, ExpeData);
+D_mm             = Data.Condition.Specifications.DiameterOfCircular; % [mm]
+D_m              = D_mm/10^3;
+L0_mm            = Data.Condition.Specifications.length0;
+L_mm             = Data.Condition.Specifications.length; % [mm]
+L_m              = L_mm/10^3;
+Q_Lmin           = ExpeData.FlowRate; % [L/min]
+Q_m3sec          = Q_Lmin/(60*10^3);
+g                = 9.81;
+delta_h0 = Data.Point0Cor;
+
+for i = 1:length(mu_Pas)
+    % Calculate dynamic viscosity coefficient
+    VarFluid.nu(i,1) = mu_Pas(i) / VarFluid.rho(i);         % 動粘性係数 [m^2/s]
+    % Calculate Reynolds
+    VarFluid.Vm(i,1) = (4*Q_m3sec(i)/(pi * D_m^2));
+    VarFluid.Re(i,1) = (VarFluid.Vm(i) * D_m/VarFluid.nu(i));
+    % Calculate Pipe friction loss coefficient
+    VarFluid.delta_h(i,1) = ((ExpeData.Upstream(i) - ExpeData.Downstream(i) - delta_h0)*sin(pi/6))/10^3;
+    VarFluid.lambda(i,1) = VarFluid.delta_h(i) * D_m/L_m * 2*g / VarFluid.Vm(i)^2;
+end
+
+end
+
